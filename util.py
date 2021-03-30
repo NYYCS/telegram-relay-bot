d8daf29298485c5b2dbf6f8a3e7b68a9e3d1ac29
NYYCS <nyystu@gmail.com>
2021-03-30 12:21:02 +0800

    removed redundancies

--- util.py
+++ util.py
@@ -5,21 +5,6 @@ import ruamel.yaml
 yaml = ruamel.yaml.YAML()
 
 
-class cached_property:
-    def __init__(self, function):
-        self.function = function
-        self.__doc__ = getattr(function, '__doc__')
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-
-        value = self.function(instance)
-        setattr(instance, self.function.__name__, value)
-
-        return value
-
-
 def shuffled(l):
     copy = l.copy()
     random.shuffle(copy)
